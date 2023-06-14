2128d0507a9c70c30531c470523abbba5aaba2cd
miyu-wl <tkgth0725@gmail.com>
2023-06-14 21:30:02 +0900

    ABC065

--- ABC065/C.c
+++ ABC065/C.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#define D 1000000000 + 7
 
 int	main() {
-	size_t	n, m;
+	size_t	n[2];
 	size_t	count = 1;
-	scanf("%zu %zu", &n, &m);
-	if ((n > m && n - m > 1) ||(m > n && m - n > 1)) {
-		printf("0\n");
-		return 0;
+	for (int i = 0; i < 2; i ++)
+		scanf("%zu", &n[i]);
+	for (int i = 0; i < 2; i ++) {
+		if ((n[i] > n[!i] && n[i] - n[!i] > 1)) {
+			printf("0\n");
+			return 0;
+		}
 	}
-	if (n == m)
+	if (n[0] == n[1])
 		count = 2;
-	while (n)
-		count *= n--;
-	while (m)
-		count *= m--;
+	for (int i = 0; i < 2; i ++) {
+		while (n[i]) {
+			count *= n[i]--;
+			if (count > D)
+				count %= D;
+		}
+	}
 	printf("%zu\n", count);
 	return 0;
 }
