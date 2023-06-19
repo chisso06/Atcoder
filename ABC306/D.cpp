1eec63447f863f1ad03cb95cb8b7db7dadb63b5b
miyu-wl <tkgth0725@gmail.com>
2023-06-19 22:40:00 +0900

    ABC036

--- ABC306/D.cpp
+++ ABC306/D.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unistd.h>
 #define rep(i, n) for (int i = 0; i < n; i ++)
 using namespace std;
 
 int	main() {
 	int							n;
+	cin >> n;
 	long long					sum = 0;
 	vector<long long>			max_f(2);
 	vector< vector<long long> >	f(n, vector<long long>(2));
 	// vector< vector<long long> >	f(2, vector<long long>(n));
 
-	cin >> n;
 	rep(i, n + 1) {
-		if (!i) {
-			max_f[f[i][0]] = f[i][1];
-			if (!f[i][0])
-				max_f[1] = 0;
-		}
 		if (i < n) {
 			cin >> f[i][0] >> f[i][1];
 			if (!f[i][0] && f[i][1] > 0)
 				sum += f[i][1];
 		}
-		if ((i == n || (i && f[i][0])) && !f[i - 1][0])
+		if (i == n && f[n - 1][0] && f[n - 1][1] > 0)
+			sum += max_f[1];
+		else if ((i == n || (i && f[i][0])) && !f[i - 1][0])
 			if (max_f[1] > 0 && max_f[0] + max_f[1] > 0)
 				sum += max_f[1] + max_f[0] * (max_f[0] < 0);
-		if (i != n && i && f[i][0] != f[i - 1][0])
+		if (!i && !f[i][0])
+			max_f[1] = 0;
+		if (!i || (i < n && f[i][0] != f[i - 1][0]))
 			max_f[f[i][0]] = f[i][1];
-		else if (i != n)
+		else if (i < n)
 			max_f[f[i][0]] = max(max_f[f[i][0]], f[i][1]);
 	}
 	cout << sum << endl;
