1477aa9a38c35854951c200d4c7ecb64a5d9baa8
miyu-wl <tkgth0725@gmail.com>
2023-07-21 18:53:56 +0900

    ABC

--- ABC/D.cpp
+++ ABC/D.cpp
@@ -3,9 +3,10 @@
 #include <set>
 #include <algorithm>
 #define ll long long
-#define rep(i, s, n) for (ll i = s; i < n; i ++)
-#define rrep(i, s, n) for (ll i = n - 1; i >= s; i --)
+#define rep(i, n) for (ll i = 0; i < n; i ++)
+#define rrep(i, n) for (ll i = n - 1; i >= 0; i --)
 #define find(list, value) find(list.begin(), list.end(), value)
+#define sort(list) sort(list.begin(), sort.end())
 using namespace std;
 
 int	main() {
